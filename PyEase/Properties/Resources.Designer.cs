3a8686aebde7e285fcb87035a165ca28f9c0cddc
CodeDisease <deacontaylor098@gmail.com>
2021-09-03 16:56:50 +1000

    some stuff

--- PyEase/Properties/Resources.Designer.cs
+++ PyEase/Properties/Resources.Designer.cs
@@ -39,7 +39,7 @@ namespace PyEase.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Python_IDE.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("PyEase.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
---
+++ PyEase/RoundedButtonIcon.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using FontAwesome.Sharp;
+
+namespace PyEase
+{
+    class RoundedButtonIcon : IconButton
+    {
+        public RoundedButtonIcon()
+        {
+
+        }
+
+        private Color _backcolor = Color.FromArgb(50, 50, 50);
+
+        public Color Backcolor
+        {
+            get => _backcolor;
+            set
+            {
+                foreach (Control c in Controls)
+                    c.BackColor = value;
+                _backcolor = value;
+                Invalidate();
+            }
+        }
+
+        protected override void OnPaint(PaintEventArgs pevent)
+        {
+            pevent.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+            GraphicsPath path = Drawing.RoundedRect(new Rectangle(0, 0, Width - 1, Height - 1), 10);
+            pevent.Graphics.FillPath(new SolidBrush(Backcolor), path);
+            pevent.Graphics.DrawPath(new Pen(Backcolor), path);
+
+            base.OnPaint(pevent);
+        }
+    }
+}
